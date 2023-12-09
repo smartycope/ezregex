f7e54760d4bb70cdf5bee90880b03802265abe90
Copeland <smartycope@gmail.com>
2023-12-08 17:49:48 -0700

    The flags group description was labeled as misc

--- ezregex/__init__.py
+++ ezregex/__init__.py
@@ -142,7 +142,7 @@ __docs__ = {
     "groups_docs": {
         'positionals': "These differentiate the *string* starting with a sequence, and a *line* starting with a sequence. Do note that the start of the string is also the start of a line. These can also be called without parameters to denote the start/end of a string/line without something specific having to be next to it.",
         'replacement': "In the intrest of \"I don't want to think about any syntax at all\", I have included replace members. Do note that they are not interoperable with the other EZRegexMembers, and can only be used with other strings and each other.",
-        'misc':        "These shadow python regex flags, and can just as easily be specified directly to the re library instead. They're provided here for compatibility with other regex dialects. See https://docs.python.org/3/library/re.html#flags for details",
+        'flags':        "These shadow python regex flags, and can just as easily be specified directly to the re library instead. They're provided here for compatibility with other regex dialects. See https://docs.python.org/3/library/re.html#flags for details",
         'premade':     "These are some useful combinations that may be commonly used. They are not as stable, and may be changed and added to in later versions to make them more accurate"
     },
     "operator_docs": {
