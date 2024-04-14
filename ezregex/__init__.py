50930d18f919239b98c021f6ccc34d8b9c0f997a
Copeland Carter <smartycope@gmail.com>
2024-04-14 02:00:29 -0600

    bumped to 1.9.0 and removed rich dependancy

--- ezregex/__init__.py
+++ ezregex/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ A readable and intuitive way to generate Regular Expressions """
-__version__ = '1.7.0'
+__version__ = '1.9.0'
 
 from .invert import invert
 from .EZRegex import EZRegex
@@ -8,6 +8,7 @@ from .EZRegex import EZRegex
 from . import generate
 from .generate import generate_regex
 from .python import *
+from .api import api
 # We have to import these manually cause they start with __
-from .python import __docs__, __groups__
-from re import RegexFlag, escape
+# from .python import __docs__, __groups__
+# from re import RegexFlag, escape
