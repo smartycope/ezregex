e5ca609b6dc1107edf3c71823ab1a0639cf73486
Copeland Carter <smartycope@gmail.com>
2024-04-14 13:11:16 -0600

    Removed specs folder in favor of dialects.py

--- ezregex/specs/__init__.py
+++
@@ -1,8 +0,0 @@
-from .python import spec as python_spec
-from .javascript import spec as javascript_spec
-
-# This is the final say of what's supported
-dialects = {
-    'python': python_spec,
-    'javascript': javascript_spec,
-}
--- ezregex/specs/python.py
+++
@@ -1,6 +0,0 @@
-spec = {
-    "beginning": '',
-    "end": '',
-    'flag_func': lambda final, flags: f'(?{flags}){final}',
-    'escape_chars': b'()[]{}?*+-|^$\\.&~# \t\n\r\v\f',
-}
