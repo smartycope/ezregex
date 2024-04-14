53485b2916ca441c23c77def0c66c4475202b700
Copeland Carter <smartycope@gmail.com>
2024-04-14 13:16:07 -0600

    Bumped to version 1.9.1, removed extraneous imports

--- ezregex/__init__.py
+++ ezregex/__init__.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 """ A readable and intuitive way to generate Regular Expressions """
-__version__ = '1.9.0'
+__version__ = '1.9.1'
 
-from .invert import invert
-from .EZRegex import EZRegex
 # Import this as a submodule
 from . import generate
-from .generate import generate_regex
-from .python import *
 from .api import api
-# We have to import these manually cause they start with __
-# from .python import __docs__, __groups__
-# from re import RegexFlag, escape
+from .EZRegex import EZRegex
+from .generate import generate_regex
+from .invert import invert
+from .python import *
