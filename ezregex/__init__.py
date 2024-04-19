18367ec9be459cabfa9da67fd3ca7d77608eb7c0
Copeland Carter <smartycope@gmail.com>
2024-04-19 00:50:38 -0600

    bump to v1.10.0

--- ezregex/__init__.py
+++ ezregex/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ A readable and intuitive way to generate Regular Expressions """
-__version__ = '1.9.5'
+__version__ = '1.10.0'
 
 # Import this as a submodule
 from . import generate
