6cf9e912982f1e7b7ec35af7add1c50dce576a06
Copeland Carter <smartycope@gmail.com>
2024-06-03 19:11:47 -0600

    The other dialects are now accessible

--- ezregex/__init__.py
+++ ezregex/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ A readable and intuitive way to generate Regular Expressions """
-__version__ = '2.2.0'
+__version__ = '2.2.1'
 
 # Import this as a submodule
 from . import generate
@@ -8,4 +8,8 @@ from .api import api
 from .EZRegex import EZRegex
 from .generate import generate_regex
 from .invert import invert
+# Python is the default
 from .python import *
+from . import R
+from . import javascript
+from . import perl
